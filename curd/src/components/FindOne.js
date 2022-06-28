d02c86445f7288751736f27bddc05bc3b48381cb
Abhitabh Kumar Pandey <78535234+abhitabh-kumar@users.noreply.github.com>
2022-06-29 00:46:55 +0530

    cors

--- curd/src/components/FindOne.js
+++ curd/src/components/FindOne.js
@@ -26,6 +26,7 @@ const FindOne = () => {
         const response=await fetch("http://ec2-54-187-83-232.us-west-2.compute.amazonaws.com:9999/api/findOne",{
         headers:{
           'Content-Type':'application/json',
+             'Access-Control-Allow-Origin':"*"
         },  
         body: JSON.stringify({
             userName,
